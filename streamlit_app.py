13dfb03b79a7ca8bf88fe511eacbe97c12fd2be7
nikkeyvatsal <144482214+nikkeyvatsal@users.noreply.github.com>
2023-09-10 00:50:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,35 @@
+import streamlit
+streamlit.title('My new code page')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+
+
+
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response);
+
+streamlit.header("Fruityvice Fruit Advice!")
+
+# write your own comment -data is normalized
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - normalizied as fruit vice
+streamlit.dataframe(fruityvice_normalized)
+
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+
+
 import snowflake.connector
