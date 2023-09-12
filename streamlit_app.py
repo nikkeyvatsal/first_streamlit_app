586829991ec76376858a3da14d558dafaa973ccf
nikkeyvatsal <144482214+nikkeyvatsal@users.noreply.github.com>
2023-09-13 01:03:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
+
 import streamlit
+import pandas
+import requests
+import snowflake.connectors
+import urllib.error import URLerror
 streamlit.title('My new code page')
 
 streamlit.header('Breakfast Menu')
@@ -7,7 +12,7 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 
-import pandas
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -15,7 +20,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
 
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response);
 
@@ -31,8 +36,8 @@ streamlit.dataframe(fruityvice_normalized)
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-
-import snowflake.connector
+streamlit.stop()
+#import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
