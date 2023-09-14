41e60616a4746dc25ff0e733858b9138487433b4
nikkeyvatsal <144482214+nikkeyvatsal@users.noreply.github.com>
2023-09-14 18:49:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,6 @@ if streamlit.button('Add a Fruit to the list'):
    streamlit.text(back_from_function)
 
 
-  def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
     return my_cur.fetchall()
