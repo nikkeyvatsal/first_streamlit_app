5856945033033b3a87f9fd241cc56fdb0293e92b
nikkeyvatsal <144482214+nikkeyvatsal@users.noreply.github.com>
2023-09-14 10:07:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,8 +60,8 @@ my_cur.execute("insert into fruit_load_list values ('From streamlit')")
 
 streamlit.header('Fruityvice Fruit Advice!')
 try:
-fruit_choice = streamlit.text_input("what fruit would you like information about?')
-if not fruit_choice:
+ = .t("')
+if 
 (streamlit.error_"Please select a fruit to get information")
 else:
 fruitvice_response= request.get("https://fruityvice.com/api/fruit/" +fruit_choice)
@@ -70,6 +70,31 @@ streamlit.dataframe(fruityvice_normalized)
 
 except URLError as e;
 streamlit.error()
+
+import requests
+
+try:
+    # Code that might raise an exception
+    fruit_choice = streamlit.text_input("what fruit would you like information about?")
+    
+    if not fruit_choice:
+        fruit_choice = response.json()
+        
+        # Check if the 'name' key exists in the JSON response
+        if 'name' in data:
+            fruit_name = data['name']
+            print(f"The fruit's name is: {fruit_name}")
+        else:
+          fruitvice_response= request.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+          fruityvice_normalized = pandas.json_normalize(fruitvice_response.json())
+          streamlit.dataframe(fruityvice_normalized)
+          
+  
+        
+except requests.exceptions.RequestException as e:
+    print(f"An error occurred while making the request: {e}")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
 
 
 
