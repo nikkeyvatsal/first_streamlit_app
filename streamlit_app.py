37e7f3c1fcf16ad7092e2c44b729cd5e7ceaded6
nikkeyvatsal <144482214+nikkeyvatsal@users.noreply.github.com>
2023-09-14 10:02:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,29 +58,18 @@ my_cur.execute("insert into fruit_load_list values ('From streamlit')")
 
 
 
-import requests
 streamlit.header('Fruityvice Fruit Advice!')
-
 try:
-    # Code that might raise an exception
-    fruit_choice = streamlit.get("https://www.fruityvice.com/api/fruit/apple")
-    
-    if response.status_code == 200:
-        data = response.json()
-        
-        # Check if the 'name' key exists in the JSON response
-        if 'name' in data:
-            fruit_name = data['name']
-            print(f"The fruit's name is: {fruit_name}")
-        else:
-            print("The 'name' key is not found in the response JSON.")
-    else:
-        print(f"Request failed with status code {response.status_code}")
-        
-except requests.exceptions.RequestException as e:
-    print(f"An error occurred while making the request: {e}")
-except Exception as e:
-    print(f"An unexpected error occurred: {e}")
+fruit_choice = streamlit.text_input("what fruit would you like information about?')
+if not fruit_choice:
+(streamlit.error_"Please select a fruit to get information")
+else:
+fruitvice_response= request.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+fruityvice_normalized = pandas.json_normalize(fruitvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
+
+except URLError as e;
+streamlit.error()
 
 
 
