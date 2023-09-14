51c416a0b2f877594d7d7ff42273beecba3c52e5
nikkeyvatsal <144482214+nikkeyvatsal@users.noreply.github.com>
2023-09-14 11:03:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,7 +90,7 @@ if st.button("Get Information"):
 
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
-  my_cur.execute("select * from fruit_load_list")
+  my_cur.execute('select * from fruit_load_list')
   return my_cur.fetchall()
 
 if streamlit.button('Get Fruitload list'):
