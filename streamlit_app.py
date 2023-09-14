ebf1a88861ec81fecb0f0e1d0be435c9616fea47
nikkeyvatsal <144482214+nikkeyvatsal@users.noreply.github.com>
2023-09-14 10:25:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 streamlit.stop()
-#import snowflake.connector
+import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -55,82 +55,18 @@ streamlit.dataframe(my_data_row)
 
 my_cur.execute("insert into fruit_load_list values ('From streamlit')")
 
-
-
-
-
-import requests
 streamlit.header('Fruityvice Fruit Advice!')
 try:
-    # Code that might raise an exception
-    fruit_choice = streamlit.text_input("what fruit would you like information about?")
-    
-    if not fruit_choice:
-        fruit_choice = response.json()
-        
-        # Check if the 'name' key exists in the JSON response
-        if 'name' in data:
-            fruit_name = data['name']
-            print(f"The fruit's name is: {fruit_name}")
-        else:
-          fruitvice_response= request.get("https://fruityvice.com/api/fruit/" +fruit_choice)
-          fruityvice_normalized = pandas.json_normalize(fruitvice_response.json())
-          streamlit.dataframe(fruityvice_normalized)
-          
-  
-        
-except requests.exceptions.RequestException as e:
-    print(f"An error occurred while making the request: {e}")
-except Exception as e:
-    print(f"An unexpected error occurred: {e}")
+  fruit_choice = streamlit.text_input("what fruit would you like information about?')
+  if not fruit_choice:
+  streamlit.error("Please select a fruit to get information.")
+else:
+ fruitvice_response= request.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+ fruityvice_normalized = pandas.json_normalize(fruitvice_response.json())
+ streamlit.dataframe(fruityvice_normalized)
+
+ except URLError as e;
 
 
-import tkinter as tk
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
 
 
-def load_fruits():
-    try:
-        # Connect to Snowflake
-        conn = snowflake.connector.connect(**snowflake_params)
-
-        # Create a cursor
-        cursor = conn.cursor()
-
-        # Execute a SQL query
-        cursor.execute("SELECT fruit_name FROM fruits")
-
-        # Fetch the result
-        result = cursor.fetchall()
-
-        # Display the result
-        result_label.config(text=f"Fruit List: {', '.join([row[0] for row in result])}")
-
-    except snowflake.connector.errors.ProgrammingError as e:
-        result_label.config(text=f"Snowflake Error: {e}")
-    except Exception as e:
-        result_label.config(text=f"An unexpected error occurred: {e}")
-    finally:
-        # Close the cursor and connection
-        cursor.close()
-        conn.close()
-
-# Create the main window
-root = tk.Tk()
-root.title("Snowflake Fruit Query")
-
-# Create a button to load fruits
-load_button = tk.Button(root, text="Load Fruits", command=load_fruits)
-load_button.pack()
-
-# Create a label to display the result
-result_label = tk.Label(root, text="")
-result_label.pack()
-
-# Start the GUI main loop
-root.mainloop()
