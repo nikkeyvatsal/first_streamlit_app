7bdc5cfa5a6469ac91d60026ab2e793dc3843914
nikkeyvatsal <144482214+nikkeyvatsal@users.noreply.github.com>
2023-09-14 10:30:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,60 @@ if st.button("Get Information"):
             st.error(f"An unexpected error occurred: {e}")
     else:
         st.error("Please select a fruit to get information.")
+
+
+
+
+import tkinter as tk
+import snowflake.connector
+
+# Snowflake connection parameters
+snowflake_params = {
+    'account': 'your_account_url',
+    'user': 'your_username',
+    'password': 'your_password',
+    'warehouse': 'your_warehouse',
+    'database': 'your_database',
+    'schema': 'your_schema'
+}
+
+def load_fruits():
+    try:
+        # Connect to Snowflake
+        conn = snowflake.connector.connect(**snowflake_params)
+
+        # Create a cursor
+        cursor = conn.cursor()
+
+        # Execute a SQL query
+        cursor.execute("SELECT fruit_name FROM fruits")
+
+        # Fetch the result
+        result = cursor.fetchall()
+
+        # Display the result
+        result_label.config(text=f"Fruit List: {', '.join([row[0] for row in result])}")
+
+    except snowflake.connector.errors.ProgrammingError as e:
+        result_label.config(text=f"Snowflake Error: {e}")
+    except Exception as e:
+        result_label.config(text=f"An unexpected error occurred: {e}")
+    finally:
+        # Close the cursor and connection
+        cursor.close()
+        conn.close()
+
+# Create the main window
+root = tk.Tk()
+root.title("Snowflake Fruit Query")
+
+# Create a button to load fruits
+load_button = tk.Button(root, text="Load Fruits", command=load_fruits)
+load_button.pack()
+
+# Create a label to display the result
+result_label = tk.Label(root, text="")
+result_label.pack()
+
+# Start the GUI main loop
+root.mainloop()
