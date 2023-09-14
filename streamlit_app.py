4124a81c094a9831cb893020f690626d91fddc13
nikkeyvatsal <144482214+nikkeyvatsal@users.noreply.github.com>
2023-09-14 10:34:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,46 +31,34 @@ if st.button("Get Information"):
 
 
 
-import snowflake.connector
 
-
-def load_fruits():
+def get_fruityvice_data(fruit_name):
     try:
-        # Connect to Snowflake
-        conn = snowflake.connector.connect(**snowflake_params)
+        # Code for fetching fruit information
+        response = requests.get(f"https://www.fruityvice.com/api/fruit/{fruit_name}")
+        data = response.json()
 
-        # Create a cursor
-        cursor = conn.cursor()
+        # Check if the response contains data
+        if 'name' in data:
+            fruit_name = data['name']
+            return f"The fruit's name is: {fruit_name}"
+        else:
+            return "The 'name' key is not found in the response JSON."
 
-        # Execute a SQL query
-        cursor.execute("SELECT fruit_name FROM fruits")
-
-        # Fetch the result
-        result = cursor.fetchall()
-
-        # Display the result
-        result_label.config(text=f"Fruit List: {', '.join([row[0] for row in result])}")
-
-    except snowflake.connector.errors.ProgrammingError as e:
-        result_label.config(text=f"Snowflake Error: {e}")
+    except requests.exceptions.RequestException as e:
+        return f"An error occurred while making the request: {e}"
     except Exception as e:
-        result_label.config(text=f"An unexpected error occurred: {e}")
-    finally:
-        # Close the cursor and connection
-        cursor.close()
-        conn.close()
+        return f"An unexpected error occurred: {e}"
 
-# Create the main window
-root = tk.Tk()
-root.title("Snowflake Fruit Query")
+st.title("Fruityvice App")
 
-# Create a button to load fruits
-load_button = tk.Button(root, text="Load Fruits", command=load_fruits)
-load_button.pack()
+# Create a dropdown for selecting a fruit
+selected_fruit = st.selectbox("Select a fruit:", ["apple", "banana", "cherry"])
 
-# Create a label to display the result
-result_label = tk.Label(root, text="")
-result_label.pack()
-
-# Start the GUI main loop
-root.mainloop()
+# Create a button to fetch fruit information
+if st.button("Get Information"):
+    if selected_fruit:
+        result = get_fruityvice_data(selected_fruit)
+        st.write(result)
+    else:
+        st.error("Please select a fruit to get information.")
